35ed786802fe59b1a43ad0c768e25aef949ab304
chriszhang94 <chris@quantumsds.com>
2021-02-26 18:56:46 -0500

    models update

--- model/model.go
+++ model/model.go
@@ -6,3 +6,15 @@ type Problem struct {
 	Desc string `json:"desc"`
 	Diff string `json:"diff"`
 }
+
+type User struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+	Password string `json:"password"`
+	Role int32 `json:"role"`
+}
+
+type Submit struct {
+	ProblemId string `json:"id"`
+	UserId string `json:"userId"`
+}
